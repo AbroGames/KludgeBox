bddf3f37ade5cb3dbfac16a68b5d546ad69af419
Bronuh <bronyx@yandex.ru>
2025-12-27 20:06:12 +0300

    Add scene service injection test

---
+++ KludgeTests/Tests/DiTests/Tests/SceneServiceInjection.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Godot;
+using KludgeBox.DI.Requests.SceneServiceInjection;
+using KludgeBox.Testing;
+using KludgeBox.Testing.Asserting;
+using static KludgeTests.Tests.DiTests.LocalServices;
+
+namespace KludgeTests.Tests.DiTests;
+
+public partial class SceneServiceInjection : TestNode
+{
+    public override string TestName => "DI Tests: scene services injection";
+
+    [Test]
+    public void InjectSimpleService()
+    {
+        var serviceParent = new ServiceProviderParent();
+        var someOtherServiceParent = new SomeOtherServiceProviderParent();
+        var child = new ChildNodeWithServices();
+        
+        someOtherServiceParent.AddChild(serviceParent);
+        serviceParent.AddChild(child);
+        
+        Di.Process(child);
+        
+        Assert.IsNotNull(child.Service);
+        Assert.IsNotNull(child.SomeOtherService);
+    }
+
+    internal partial class ChildNodeWithServices : DiTestTypes.ChildNode
+    {
+        [SceneService] public SomeService Service { get; private set; }
+        [SceneService] public SomeOtherService SomeOtherService { get; private set; }
+    }
+    
+    internal partial class ServiceProviderParent : DiTestTypes.ParentNode,  ISceneServiceProvider
+    {
+        public object GetService(Type serviceType)
+        {
+            if (serviceType == typeof(SomeService))
+            {
+                return new SomeService();
+            }
+
+            return null;
+        }
+    }
+    
+    internal partial class SomeOtherServiceProviderParent : DiTestTypes.ParentNode,  ISceneServiceProvider
+    {
+        public object GetService(Type serviceType)
+        {
+            if (serviceType == typeof(SomeOtherService))
+            {
+                return new SomeOtherService();
+            }
+
+            return null;
+        }
+    }
+
+    internal class SomeService;
+    internal class SomeOtherService;
+}
