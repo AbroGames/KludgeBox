6738b0b7455be1bcb60550e12b4ca8388f49bc33
Evgeniy Serov <kejik148@gmail.com>
2026-02-21 01:14:17 +0500

    Add ExecutingAssemblyCacheService

---
+++ KludgeBox/Core/ExecutingAssemblyCacheService.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+namespace KludgeBox.Core;
+
+public class ExecutingAssemblyCacheService
+{
+    public IReadOnlyList<Type> Types => _executingAssemblyTypes.AsReadOnly();
+    private List<Type> _executingAssemblyTypes;
+
+    public void Init(Assembly executingAssembly)
+    {
+        _executingAssemblyTypes = executingAssembly.GetTypes().ToList();
+    }
+}
